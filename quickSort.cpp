5e1ec1bc8c1ca5e743f5922355637d24a67f39aa
gaurav79829 <56987190+gaurav79829@users.noreply.github.com>
2021-10-30 10:57:25 +0530

    Update quickSort.cpp
    cout << "Sorted array is: \n";

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,4 +1,5 @@
 /* C++ implementation of QuickSort */
+
 #include <bits/stdc++.h> 
 using namespace std; 
 
@@ -63,7 +64,7 @@ int main()
 	int arr[] = {10, 7, 8, 9, 1, 5}; 
 	int n = sizeof(arr) / sizeof(arr[0]); 
 	quickSort(arr, 0, n - 1); 
-	cout << "Sorted array: \n"; 
+	cout << "Sorted array is: \n"; 
 	printArray(arr, n); 
 	return 0; 
 } 
